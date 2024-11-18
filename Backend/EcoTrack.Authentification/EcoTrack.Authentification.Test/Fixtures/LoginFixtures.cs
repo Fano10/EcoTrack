776a3b4fa05a9c7a6e8fd8ccf9dc1b1f030c50f5
Rakotondrabe Miora Fanomezana <rsjasonfano@gmail.com>
2024-11-18 17:42:33 -0500

    add first test and features

---
+++ Backend/EcoTrack.Authentification/EcoTrack.Authentification.Test/Fixtures/LoginFixtures.cs
@@ -0,0 +1,22 @@
+﻿using EcoTrack.Authentification.API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoTrack.Authentification.Test.Fixtures
+{
+    public static class LoginFixtures
+    {
+        public static UserLoginDto getUser()
+        {
+            UserLoginDto user = new UserLoginDto()
+            {
+                Email = "rsjasonfano@gmail.com",
+                Password ="1234"
+            };
+            return user;
+        }
+    }
+}
