c2da039c099a47b2102eb8b48fa76f21960409c6
Rakotondrabe Miora Fanomezana <rsjasonfano@gmail.com>
2024-11-19 10:54:02 -0500

    add first authentification in loginController

--- Backend/EcoTrack.Authentification/EcoTrack.Authentification.API/Controllers/LoginController.cs
+++ Backend/EcoTrack.Authentification/EcoTrack.Authentification.API/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using EcoTrack.Authentification.API.DTO;
+﻿using EcoTrack.Authentification.API.Data;
+using EcoTrack.Authentification.API.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EcoTrack.Authentification.API.Controllers
@@ -7,10 +8,23 @@ namespace EcoTrack.Authentification.API.Controllers
     [Route("[Controller]")]
     public class LoginController : Controller
     {
+        private UserContext _userContext;
+        public LoginController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
         [HttpPost]
         public IActionResult Connection(UserLoginDto user)
         {
-            return new OkResult();
+            var userDb = _userContext.Users.SingleOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (userDb == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok();
+            }
         }
     }
 }
--- Backend/EcoTrack.Authentification/EcoTrack.Authentification.Test/System/Controller/TestLoginController.cs
+++ Backend/EcoTrack.Authentification/EcoTrack.Authentification.Test/System/Controller/TestLoginController.cs
@@ -1,8 +1,11 @@
 ﻿using EcoTrack.Authentification.API.Controllers;
+using EcoTrack.Authentification.API.Data;
 using EcoTrack.Authentification.API.DTO;
+using EcoTrack.Authentification.API.Model;
 using EcoTrack.Authentification.Test.Fixtures;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -14,17 +17,108 @@ namespace EcoTrack.Authentification.Test.System.Controller
 {
     public class TestLoginController
     {
-        [Fact]
-        public void Get_OnSuccess_ReturnsStatusCode200()
+        //Faux par ce que tu ne peux pas simuler directement le comportement du LINQ sur la base de donnee
+        public void FalseGet_OnSuccess_ReturnsStatusCode200()
         {
             // Arrange : Dans cette étape, on met en place tout ce qui est nécessaire pour exécuter le test
-            var sut = new LoginController();
-            var mockUser = LoginFixtures.getUser();
+            var mockDbContext = new Mock<UserContext>();
+            var realUser = LoginFixtures.getUserInputTrue();
+            mockDbContext
+                .Setup(service => service.Users.SingleOrDefault(u => u.Email == realUser.Email && u.Password == realUser.Password));
+            var mockUser = LoginFixtures.getUserInputTrue();
+            var sut = new LoginController(mockDbContext.Object);
             // Act : Ici, on exécute l'action ou le comportement à tester
             var result = (StatusCodeResult)sut.Connection(mockUser);
 
             // Assert: Enfin, on vérifie que le comportement ou le résultat correspond à l'attente
             result.StatusCode.Should().Be(200);
         }
+        [Fact]
+        public void TrueGet_OnSuccess_ReturnsStatusCode200()
+        {
+            // Arrange : Mise en place des données et des mocks
+
+            // Mock du DbSet<User>
+            var mockDbSet = LoginFixtures.mockDbSet();
+
+            // Mock du UserContext
+            var mockDbContext = new Mock<UserContext>();
+            mockDbContext.Setup(c => c.Users).Returns(mockDbSet.Object);
+
+            var mockUserInput = LoginFixtures.getUserInputTrue();
+            var sut = new LoginController(mockDbContext.Object);
+
+            // Act : Exécution de la méthode à tester
+            var result = (StatusCodeResult)sut.Connection(mockUserInput);
+
+            // Assert : Vérification des résultats
+            result.StatusCode.Should().Be(200);
+        }
+        [Fact]
+        public void Get_NotFound_ReturnsStatusCode404_WrongPassword()
+        {
+            // Arrange : Mise en place des données et des mocks
+
+            // Mock du DbSet<User>
+            var mockDbSet = LoginFixtures.mockDbSet();
+
+            // Mock du UserContext
+            var mockDbContext = new Mock<UserContext>();
+            mockDbContext.Setup(c => c.Users).Returns(mockDbSet.Object);
+
+            var mockUserInput = LoginFixtures.getUserInputPasswordFalse();
+            var sut = new LoginController(mockDbContext.Object);
+
+            // Act : Exécution de la méthode à tester
+            var result = (StatusCodeResult)sut.Connection(mockUserInput);
+
+            // Assert : Vérification des résultats
+            result.StatusCode.Should().Be(404);
+        }
+        [Fact]
+        public void Get_NotFound_ReturnsStatusCode404_WrongEmail()
+        {
+            // Arrange : Mise en place des données et des mocks
+
+            // Mock du DbSet<User>
+            var mockDbSet = LoginFixtures.mockDbSet();
+
+            // Mock du UserContext
+            var mockDbContext = new Mock<UserContext>();
+            mockDbContext.Setup(c => c.Users).Returns(mockDbSet.Object);
+
+            var mockUserInput = LoginFixtures.getUserInputEmailFalse();
+            var sut = new LoginController(mockDbContext.Object);
+
+            // Act : Exécution de la méthode à tester
+            var result = (StatusCodeResult)sut.Connection(mockUserInput);
+
+            // Assert : Vérification des résultats
+            result.StatusCode.Should().Be(404);
+        }
+        [Fact]
+        public void Get_NotFound_ReturnsStatusCode404_AllWrong()
+        {
+            // Arrange : Mise en place des données et des mocks
+
+            // Mock du DbSet<User>
+            var mockDbSet = LoginFixtures.mockDbSet();
+
+            // Mock du UserContext
+            var mockDbContext = new Mock<UserContext>();
+            mockDbContext.Setup(c => c.Users).Returns(mockDbSet.Object);
+
+            var mockUserInput = LoginFixtures.getUserInputAllFalse ();
+            var sut = new LoginController(mockDbContext.Object);
+
+            // Act : Exécution de la méthode à tester
+            var result = (StatusCodeResult)sut.Connection(mockUserInput);
+
+            // Assert : Vérification des résultats
+            result.StatusCode.Should().Be(404);
+        }
+
+
+
     }
 }
